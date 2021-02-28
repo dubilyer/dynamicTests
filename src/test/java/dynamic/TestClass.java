5acb39566a2c735c4fc71ca9ab974c66659ef5c7
dubilyer <eduard.dubilyer@finastra.com>
2021-02-28 12:38:45 +0200

    [] -> ...

--- src/test/java/dynamic/TestClass.java
+++ src/test/java/dynamic/TestClass.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import org.junit.jupiter.api.function.Executable;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -13,12 +16,14 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 public class TestClass {
 
-    Step step =  ()->  System.out.println("a");
-    Step step1 = ()-> System.out.println("b");
-    Step step2 = ()-> System.out.println("c");
+    private final Logger logger = LoggerFactory.getLogger(TestClass.class);
+
+    Step step1 = ()-> logger.info(()-> "running step1");
+    Step step2 = ()-> logger.info(()-> "running step2");
+    Step step3 = ()-> logger.info(()-> "running step3");
 
     Executable getExecutable (){
-        return step.andThen(step1).andThen(step2);
+        return step1.andThen(step2).andThen(step3);
     }
 
     @TestFactory
@@ -32,8 +37,8 @@ public class TestClass {
     @TestFactory
     Collection<DynamicTest> testArray(){
         return Arrays.asList(
-                dynamicTest("testArray", combineSteps(step, step1, step2)),
-                dynamicTest("testArray2", combineSteps(step2, step1, step2, step))
+                dynamicTest("testArray", combineSteps(step1, step2, step3)),
+                dynamicTest("testArray2", combineSteps(step3, step2, step3, step1))
         );
     }
 }
